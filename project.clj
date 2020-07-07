7cf787a42311f445f828061aab1795f84afcf1a2
category <5478349+category@users.noreply.github.com>
2020-07-07 12:07:28 +0100

    Add project.clj

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject griffinbank/aws-api "0.8.456-144"
+  :resource-paths ["src" "resources"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/core.async "1.0.567"]
+                 [org.clojure/tools.logging "1.0.0"]
+                 [org.clojure/data.json "1.0.0"]
+                 [org.clojure/data.xml "0.2.0-alpha6"]
+                 [com.cognitect/http-client "0.1.104"]])
